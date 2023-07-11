1a9eed17404050f929ce71a5f111631e5c0c04e9
Nicola Racco <nicola@condense.tech>
2023-07-11 14:31:27 +0200

    allow to filter resources to operate

--- src/commands/common.ts
+++ src/commands/common.ts
@@ -1,11 +1,31 @@
 import { Auth, Directus, TypeMap } from '@directus/sdk';
 
+export enum ResourceType {
+  SCHEMA = 'schema',
+  FOLDERS = 'folders',
+  PERMISSIONS = 'permissions',
+  TRANSLATIONS = 'translations',
+}
+
 export interface BaseCommandOptions {
   token?: string;
   email?: string;
   password?: string;
   otp?: string;
   host: string;
+  only?: ResourceType[];
+  except?: ResourceType[];
+}
+
+export function getRequestedResourceTypes(opts: BaseCommandOptions): ResourceType[] {
+  const resourceTypeList = Object.values(ResourceType);
+  if (opts.only) {
+    return resourceTypeList.filter((type) => opts.only?.includes(type));
+  }
+  if (opts.except) {
+    return resourceTypeList.filter((type) => !opts.except?.includes(type));
+  }
+  return resourceTypeList;
 }
 
 export type DirectusClient = Directus<TypeMap, Auth>;
