c329dcf10f564a05705c3c08c79eff7259d0bba8
Nicola Racco <nicola.racco@gmail.com>
2023-11-28 08:33:42 +0100

    WIP: update directus-sdk (schema)

--- lib/src/commands/schema.ts
+++ lib/src/commands/schema.ts
@@ -1,42 +1,24 @@
-import axios from 'axios';
+import * as sdk from '@directus/sdk';
 import { DirectusClient } from './common';
 import { isEmpty } from 'lodash-es';
 
 export async function snapshotSchema(client: DirectusClient) {
-  const response = await axios.get(`${client.url}/schema/snapshot?export=json`, {
-    headers: {
-      Authorization: `Bearer ${await client.auth.token}`,
-      'Content-Type': 'application/json',
-    },
-  });
-  return response.data;
+  return client.request(sdk.schemaSnapshot());
 }
 
-export async function applySchemaSnapshot(client: DirectusClient, snapshot: unknown) {
+export async function applySchemaSnapshot(client: DirectusClient, snapshot: sdk.SchemaSnapshotOutput) {
   const diff = await diffSchema(client, snapshot);
   if (isEmpty(diff)) {
     console.log('  [schema] Skipping because there is no difference');
     return;
   }
-  return applySchema(client, diff.data);
+  return applySchema(client, diff);
 }
 
-async function diffSchema(client: DirectusClient, snapshot: unknown) {
-  const response = await axios.post(`${client.url}/schema/diff`, snapshot, {
-    headers: {
-      Authorization: `Bearer ${await client.auth.token}`,
-      'Content-Type': 'application/json',
-    },
-  });
-  return response.data;
+async function diffSchema(client: DirectusClient, snapshot: sdk.SchemaSnapshotOutput) {
+  return client.request(sdk.schemaDiff(snapshot));
 }
 
-async function applySchema(client: DirectusClient, diff: unknown) {
-  const response = await axios.post(`${client.url}/schema/apply`, diff, {
-    headers: {
-      Authorization: `Bearer ${await client.auth.token}`,
-      'Content-Type': 'application/json',
-    },
-  });
-  return response.data;
+async function applySchema(client: DirectusClient, diff: sdk.SchemaDiffOutput) {
+  return client.request(sdk.schemaApply(diff));
 }
